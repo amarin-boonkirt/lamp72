6f452186d160b9baf8518f88c12bf9a0728f6a29
amarin <amarin@amarin-pc>
2019-04-02 00:36:41 +0700

    new structure
    Signed-off-by: amarin <amarin@amarin-pc>

---
+++ dev-service-ctl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# docker-compose -f docker-compose.yml up --build -d
+# docker-compose -f docker-compose.yml down
+
+# docker-compose -f docker-compose.yml start -d
+# docker-compose -f docker-compose.yml stop
---
+++ production-service-ctl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# production-service-ctl.sh up/down all
+# production-service-ctl.sh up/down app
+# production-service-ctl.sh up/down tools
+
+# docker-compose -f docker-compose-production.yml up --build -d
+# docker-compose -f docker-compose-production.yml down
+
+# docker-compose -f docker-compose-production.yml start -d
+# docker-compose -f docker-compose-production.yml stop
